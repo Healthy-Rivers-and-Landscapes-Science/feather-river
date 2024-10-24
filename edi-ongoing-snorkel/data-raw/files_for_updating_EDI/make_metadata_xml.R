3513e1dd5290e54b212e0bd3ca002186d41cf39a
alvizek <avizek@flowwest.com>
2024-10-24 11:36:22 -0700

    updates links for the transition to HRL organization and repo

--- edi-ongoing-snorkel/data-raw/files_for_updating_EDI/make_metadata_xml.R
+++ edi-ongoing-snorkel/data-raw/files_for_updating_EDI/make_metadata_xml.R
@@ -4,17 +4,18 @@ library(EMLaide)
 library(readxl)
 library(EML)
 
+# Create EML file ---------------------------------------------------------
 datatable_metadata <-
   dplyr::tibble(filepath = c("data/survey_characteristics.csv",
                              "data/locations_lookup.csv",
                              "data/fish_observations.csv"),
-                attribute_info = c("data-raw/metadata/survey_characteristics_metadata.xlsx",
-                                   "data-raw/metadata/locations_lookup_metadata.xlsx",
-                                   "data-raw/metadata/fish_observations_metadata.xlsx"),
+                attribute_info = c("data-raw/files_for_updating_EDI/metadata/survey_characteristics_metadata.xlsx",
+                                   "data-raw/files_for_updating_EDI/metadata/locations_lookup_metadata.xlsx",
+                                   "data-raw/files_for_updating_EDI/metadata/fish_observations_metadata.xlsx"),
                 datatable_description = c("Survey metadata from Feather River snorkel survey data",
                                           "Location lookup for Feather River snorkel survey data",
                                           "Fish observations from Feather River snorkel survey data"),
-                datatable_url = paste0("https://raw.githubusercontent.com/FlowWest/edi-feather-snorkel/main/data/",
+                datatable_url = paste0("https://raw.githubusercontent.com/Healthy-Rivers-and-Landscapes-Science/feather-river/tree/main/edi-ongoing-snorkel/data/",
                                        c("survey_characteristics.csv",
                                          "locations_lookup.csv",
                                          "fish_observations.csv")))
@@ -22,21 +23,21 @@ datatable_metadata <-
 other_entity_metadata_1 <- list("file_name" = "unit_spatial_data.zip",
                                 "file_description" = "Kmz and GIS files for survey units",
                                 "file_type" = "zip",
-                                "physical" = create_physical("data-raw/metadata/unit_spatial_data.zip",
-                                                             data_url = "https://raw.githubusercontent.com/FlowWest/edi-feather-snorkel/main/data-raw/metadata/unit_spatial_data.zip"))
+                                "physical" = create_physical("data-raw/files_for_updating_EDI/metadata/unit_spatial_data.zip",
+                                                             data_url = "https://raw.githubusercontent.com/Healthy-Rivers-and-Landscapes-Science/feather-river/tree/main/edi-ongoing-snorkel/data-raw/files_for_updating_EDI/metadata/unit_spatial_data.zip"))
 
 other_entity_metadata_1$physical$dataFormat <- list("externallyDefinedFormat" = list("formatName" = "zip"))
 
 # save cleaned data to `data/`
-excel_path <- "data-raw/metadata/feather_ongoing_snorkel_metadata.xlsx" #TODO check this excel path/name
+excel_path <- "data-raw/files_for_updating_EDI/metadata/feather_ongoing_snorkel_metadata.xlsx"
 sheets <- readxl::excel_sheets(excel_path)
 metadata <- lapply(sheets, function(x) readxl::read_excel(excel_path, sheet = x))
 names(metadata) <- sheets
 
-abstract_docx <- "data-raw/metadata/abstract.docx"
-methods_docx <- "data-raw/metadata/methods.md"
+abstract_docx <- "data-raw/files_for_updating_EDI/metadata/abstract.docx"
+methods_docx <- "data-raw/files_for_updating_EDI/metadata/methods.md"
 
-#edi_number <- reserve_edi_id(user_id = Sys.getenv("EDI_USER_ID"), password = Sys.getenv("EDI_PASSWORD"))
+# TODO: When updating the package this version number needs to be updated.
 edi_number <- "edi.1764.1"
 
 dataset <- list() %>%
@@ -72,8 +73,14 @@ eml <- list(packageId = edi_number,
 edi_number
 EML::write_eml(eml, paste0(edi_number, ".xml"))
 EML::eml_validate(paste0(edi_number, ".xml"))
+
+
+# Evaluate EML ------------------------------------------------------------
+
+# TODO Evaluate the package on EDI to check for errors before uploading!
+
+
+# Update data on EDI ------------------------------------------------------
+
 # EMLaide::update_edi_package(Sys.getenv("edi_user_id"), Sys.getenv("edi_password"), "edi.1764.1", paste0(edi_number, ".xml"))
-# EMLaide::upload_edi_package(Sys.getenv("EDI_USER_ID"),
-#                             Sys.getenv("EDI_PASSWORD"),
-#                             "edi.1764.1.xml",
-#                             environment = "production")
+
